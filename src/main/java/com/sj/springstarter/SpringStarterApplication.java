9c1fbb18cb1954c8a1b21c797a56962dfbd70979
NIIIII99 <mystilltime@naver.com>
2018-02-20 23:32:03 +0900

    first spring boot application

---
+++ src/main/java/com/sj/springstarter/SpringStarterApplication.java
@@ -0,0 +1,15 @@
+package com.sj.springstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterApplication.class, args);
+	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringStarterApplication.class, args);
+//	}
+}
